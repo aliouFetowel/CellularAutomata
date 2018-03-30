d75dc94ea716ad5462dc65907bb593d27d7536d6
barry mamadou aliou <aliou@linux.home>
2018-03-30 16:14:24 +0200

    grille avec position

--- test/main.cc
+++ test/main.cc
@@ -1,9 +1,15 @@
 #include<iostream>
+#include<string>
+#include<fstream>
 #include"../include/Cellule.h"
 #include"../include/Grille.h"
 
 int main(){
   Grille g = Grille();
+  cout<<g<<endl;
+  g.iniRand();
+  g.ecrire();
+  g.lire();
   cout<<g<<endl;
   return 0;
 }
